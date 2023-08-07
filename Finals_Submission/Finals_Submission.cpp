cb2ed7f7d153049b6308338b6bfd6f17d11d3f2b
TC <antonio.casakit@outlook.com>
2023-08-07 17:16:15 +0800

    Added Queue and Heap Maps

--- Finals_Submission/Finals_Submission.cpp
+++ Finals_Submission/Finals_Submission.cpp
@@ -1,8 +1,16 @@
-//Create a Stack
+//Finals_Submission_for_Data_Structures
+
+/*Antonio Casakit
+09001852900
+BSIT-2nd Year
+AMACC - Las Pinas
+*/
 
 #include <iostream>
 #include <string>
-
+#include <queue>
+#include <unordered_map>
+#include <cstdlib>
 using namespace std;
 
 class Stack
@@ -96,73 +104,170 @@ public:
 
 int main()
 {
-	Stack s1;
-	int option, position, value;
+	int moption;
 	do
 	{
 		cout << "What operation do you want to perform?" << endl;
-		cout << "1. Push" << endl;
-		cout << "2. Pop" << endl;
-		cout << "3. isEmpty()" << endl;
-		cout << "4. isFull()" << endl;
-		cout << "5. peek()" << endl;
-		cout << "6. count()" << endl;
-		cout << "7. change()" << endl;
-		cout << "8. display()" << endl;
-		cout << "9. Clear Screen" << endl << endl;
-
-		cin >> option;
-		switch (option)
+		cout << "1. Stack" << endl;
+		cout << "2. Queue" << endl;
+		cout << "3. Heap Maps" << endl;
+		cout << "4. List" << endl;
+		cout << "9. Clear Screen" << endl;
+		cout << "0. Exit Program" << endl << endl;
+		cin >> moption;
+		switch (moption)
 		{
 		case 0:
 			break;
 		case 1:
-			cout << "Enter an item to push into the stack" << endl << endl;
-			cin >> value;
-			s1.push(value);
-			break;
+		{Stack s1;
+		int option, position, value;
+		do
+		{
+			cout << "What operation do you want to perform?" << endl;
+			cout << "1. Push" << endl;
+			cout << "2. Pop" << endl;
+			cout << "3. isEmpty()" << endl;
+			cout << "4. isFull()" << endl;
+			cout << "5. peek()" << endl;
+			cout << "6. count()" << endl;
+			cout << "7. change()" << endl;
+			cout << "8. display()" << endl;
+			cout << "9. Clear Screen" << endl << endl;
+			cout << "0. Previous Screen" << endl << endl;
+
+			cin >> option;
+			switch (option)
+			{
+			case 0:
+				break;
+			case 1:
+				cout << "Enter an item to push into the stack" << endl << endl;
+				cin >> value;
+				s1.push(value);
+				break;
+			case 2:
+				cout << "Pop Function Called - Popped Value: " << s1.pop() << endl << endl;
+				break;
+			case 3:
+				if (s1.isEmpty())
+					cout << "Stack is empty" << endl << endl;
+				else
+					cout << "Stack is not empty" << endl << endl;
+				break;
+			case 4:
+				if (s1.isFull())
+					cout << "Stack is full" << endl << endl;
+				else
+					cout << "Stack is not full" << endl << endl;
+				break;
+			case 5:
+				cout << "Enter the position of the item that you want to peek" << endl << endl;
+				cin >> position;
+				cout << "Peek function called - Value at position " << position << " is " << s1.peek(position) << endl << endl;
+				break;
+			case 6:
+				cout << "Count function called - The number of items in the Stack are " << s1.count() << endl << endl;
+				break;
+			case 7:
+				cout << "Change function called - " << endl << endl;
+				cout << "Enter the position of the item that you want to change: ";
+				cin >> position;
+				cout << endl;
+				cout << "Enter the value of the item you want to change: ";
+				cin >> value;
+				s1.change(position, value);
+				break;
+			case 8:
+				cout << "Display Function Called - " << endl << endl;
+				s1.display();
+				break;
+			case 9:
+				system("cls");
+				break;
+			default:
+				cout << "Enter a valid option number" << endl << endl;
+			}
+		} while (option != 0); }
+		break;
 		case 2:
-			cout << "Pop Function Called - Popped Value: " << s1.pop() << endl << endl;
-			break;
+		{queue <int> q, q1;
+		q.push(1);
+		q.push(5);
+		q.push(3);
+		q.push(6);
+		q.push(2);
+		q.push(0);
+		q.push(-1);
+		q.push(4);
+		q1 = q;
+		cout << "Queue elements are ";
+		while (!q1.empty())
+		{
+			cout << q1.front() << " ";
+			q1.pop();
+		}
+		cout << endl << "Average of all elements of the said queue is ";
+		int sum = 0;
+		int count = 0;
+		while (!q.empty())
+		{
+			sum += q.front();
+			count++;
+			q.pop();
+		}
+		double avg = (double)sum / count;
+		cout << avg << endl << endl;
+		break;
+		}
 		case 3:
-			if (s1.isEmpty())
-				cout << "Stack is empty" << endl << endl;
-			else
-				cout << "Stack is not empty" << endl << endl;
-			break;
-		case 4:
-			if (s1.isFull())
-				cout << "Stack is full" << endl << endl;
-			else
-				cout << "Stack is not full" << endl << endl;
-			break;
-		case 5:
-			cout << "Enter the position of the item that you want to peek" << endl << endl;
-			cin >> position;
-			cout << "Peek function called - Value at position " << position << " is " << s1.peek(position) << endl << endl;
-			break;
-		case 6:
-			cout << "Count function called - The number of items in the Stack are " << s1.count() << endl << endl;
-			break;
-		case 7:
-			cout << "Change function called - " << endl << endl;
-			cout << "Enter the position of the item that you want to change: ";
-			cin >> position;
-			cout << endl;
-			cout << "Enter the value of the item you want to change: ";
-			cin >> value;
-			s1.change(position, value);
-			break;
-		case 8:
-			cout << "Display Function Called - " << endl << endl;
-			s1.display();
-			break;
+		{unordered_map<int, int>myTable;
+		int elem, key;
+		int searchKey;
+		int option;
+		do
+		{
+			cout << "What operation do you want to perform?" << endl;
+			cout << "1. Insert an element into the table" << endl;
+			cout << "2. Search for an element from the key" << endl;
+			cout << "3. Delete an element using a key" << endl;
+			cout << "4. Exit" << endl;
+			cout << "0. Previous Screen" << endl << endl;
+
+			cin >> option;
+			switch (option)
+			{
+			case 0:
+				break;
+			case 1:
+				cout << "Enter element to be inserted: " << endl;
+				cin >> elem;
+				cout << "Enter key at which element to be inserted: " << endl;
+				cin >> key;
+				myTable.insert(pair<int, int>(elem, key));
+				for (auto pair : myTable)
+					cout << endl << pair.first << " - " << pair.second << endl;
+				break;
+			case 2:
+				cout << "Enter the key: " << endl;
+				cin >> searchKey;
+				cout << "Found it in " << myTable[searchKey] << endl;
+				break;
+			case 3:
+				cout << "Enter the key: " << endl;
+				cin >> searchKey;
+				myTable.erase(searchKey);
+				break;
+			case 4:
+				return 0;
+			default:
+				cout << "Enter a valid option" << endl << endl;
+			}
+		} while (option != 0); }
 		case 9:
 			system("cls");
 			break;
-		default:
-			cout << "Enter a valid option number" << endl << endl;
 		}
-	} while (option != 0);
+	} while (moption != 0);
 	return 0;
 }
